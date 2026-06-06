cfdb21644f923834f390ca11fda67313b43f8fe2
amirali <aminiamiraliamini1400@outlook.com>
2026-06-06 16:38:33 +0330

    image

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -16,8 +16,7 @@ class ImagesController extends Controller
         $hash = new Hashids();
         $files = new Files();
         $files = $files->find($hash->decode($id)[0]);
-        dd( $files);
-        $f = Storage::disk('parspack')->get(str_replace("user/","",$files->file_address));
+        $f = Storage::disk('parspack')->get($files->file_address);
         $response = Response::make($f, 200);
         $response->header('Content-Type', $files->mime_type);
         return $response;
